ff93957fb24915972cd129074807d26154580fe3
Susanna Lakey <susannalakey@gmail.com>
2020-10-06 09:27:48 -0700

    updated answer

--- zeroSum/zero-sum.js
+++ zeroSum/zero-sum.js
@@ -1,24 +1,24 @@
 // given an array of integers, print all sub-arrays with 0 sum
 
-// array = [4, 2, -3, -1, 0, 4];
-// subArrays = [
+// Input: array = [4, 2, -3, -1, 0, 4];
+// Output: subArrays = [
 //     [-3, -1, 0, 4],
 //      [0]
 //   ]
 
+const sumArray = (numbers) => numbers.reduce((sum, current) => sum + current);
+
+// declarative > imperative
 
 const zeroSum = arr => {
   const subArrays = [];
 
   for (let start = 0; start < arr.length; start++) {
-    let tempArray = [];
-    let total = 0;
     for (let end = start; end < arr.length; end++) {
-      tempArray.push(arr[end]);
-      total += arr[end];
-      if (total === 0) {
-        subArrays.push(tempArray);
-        tempArray = [];
+      const subArray = arr.slice(start, end + 1);
+      const sum = sumArray(subArray);
+      if (sum === 0) {
+        subArrays.push(subArray);
       }
     }
   }
